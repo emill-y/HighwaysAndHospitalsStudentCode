f3c01cfa1184a76edc0a64e00d05f381b9d0e765
emill-y <eishayadav@gmail.com>
2025-09-11 13:43:39 -0700

    Implemented edge case functionality-- and not much else..

--- src/HighwaysAndHospitals.java
+++ src/HighwaysAndHospitals.java
@@ -1,3 +1,8 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * Highways & Hospitals
  * A puzzle created by Zach Blick
@@ -15,6 +20,27 @@ public class HighwaysAndHospitals {
      *  hospital access for all citizens in Menlo County.
      */
     public static long cost(int n, int hospitalCost, int highwayCost, int cities[][]) {
+        // Eliminate initial edge case
+        if (hospitalCost <= highwayCost) {
+            return ((long) n * hospitalCost);
+        }
+
+        // Store potential connections for each city
+        // Array of Arraylists
+        Object[][] cityConnections = new Object[n][2];
+        for(int i = 0; i < n; i++){
+            cityConnections[i][0] = new ArrayList<Integer>();
+            cityConnections[i][1] = false;
+        }
+
+        for(int i = 0; i < cities.length; i++) {
+            //(cityConnections[cities[i][0]][0]).add(1);
+        }
+        System.out.println(Arrays.toString(cityConnections));
+
         return 0;
+//    public boolean accessHighway(){
+//
+//    }
     }
 }
